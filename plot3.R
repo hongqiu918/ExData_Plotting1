59c37871a07f8f263418ef770e11bfb0ebe36302
Hong Qiu <hongqiu918@gmail.com>
2014-06-08 14:36:31 -0400

    plot3

--- plot3.R
+++ plot3.R
@@ -6,6 +6,9 @@ y1<-household1$Sub_metering_1
 y2<-household1$Sub_metering_2
 y3<-household1$Sub_metering_3
 png(file="plot3.png", h=480, w=480)
+plot(x,y1,type="l",col="Black",ylab="Energy sub metering", xlab="")
 lines(x,y2, type="l", col="Red")
 lines(x,y3, type="l", col="Blue")
+legend("topright",lty=c(1,1,1), lwd=1, col=c("Black","Red", "Blue"), 
+       legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 dev.off()
